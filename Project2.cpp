a4c549a5428aec872c4f717b5f42511a9afa1907
RANDYzY <maximusismax@gmail.com>
2023-11-07 15:44:57 +0300

    first commit by RANDYzY

---
+++ Project2.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Buy my paket!" << endl;
+    cout << "Close eyes!" << endl;
+    cout << "Buy my pakets!" << endl;
+    //...
+}
